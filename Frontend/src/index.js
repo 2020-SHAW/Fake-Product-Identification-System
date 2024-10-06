11aab101a5014f1acd1bbc384ae447ecf954b3b7
John Mary <131287168+Okalmary254@users.noreply.github.com>
2024-10-06 15:51:45 +0300

    Update index.js

---
+++ Frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Render the React app and wrap it with Router for routing support
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
